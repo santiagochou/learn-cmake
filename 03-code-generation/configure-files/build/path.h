ce3dd51239e5bdee8fc56635cde63896bb1cdde2
Santiago Chou <xfzhou2018@gmail.com>
2020-09-26 01:34:28 +0800

    update

--- 03-code-generation/configure-files/build/path.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PATH_H__
-#define __PATH_H__
-
-// version variable that will be substitured by cmake
-// This shows an example using the @ variable type
-const char* path = "D:/Santiago/Github/learn-cmake/03-code-generation/configure-files";
-
-#endif
